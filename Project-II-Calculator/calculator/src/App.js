a6837253ad5747d375761758365fd7442028224b
Zech Drinkhall <zechdrink@aol.com>
2019-02-12 12:40:26 -0800

    added a component for numbered buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import NumberButton from './components/ButtonComponents/NumberButton';
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className ="App">
+      <div className = "firstRow">
+        <NumberButton number = "7"/>
+        <NumberButton number = "8"/>
+        <NumberButton number = "9"/>
+        <NumberButton number = "X"/>
+      </div>
+      <div className = "secondRow">
+        <NumberButton number = "4"/>
+        <NumberButton number = "5"/>
+        <NumberButton number = "6"/>
+        <NumberButton number = "-"/>
+      </div>
+      <div className = "thirdRow">
+        <NumberButton number = "1"/>
+        <NumberButton number = "2"/>
+        <NumberButton number = "3"/>
+        <NumberButton number = "+"/>
+      </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props){
+return (
+        <button id = "numberButton">{props.number}</button>
+)
+} 
+
+export default NumberButton;
